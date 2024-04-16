89d1ee0d0ab6f6ece2527e8773497f389755d321
a5hi5hx <ashishpaudel54@gmail.com>
2024-04-16 20:34:52 +0545

    minor

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Head from 'next/head';
+
+const Contact: React.FC = () => {
+    return (
+        <div>
+            <Head>
+                <title>Database page</title>
+            </Head>
+            <div>
+                <h1>Contact Me</h1>
+                <p>Loading Information............................</p>
+                <p></p>
+                <p>Feel free to contact if you encounter any <b>ERROR</b></p> 
+            </div>
+        </div>
+    );
+};
+
+export default Contact;

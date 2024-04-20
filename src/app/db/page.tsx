7770d15ed52621942d038efa3948965b1ca8b3fd
a5hi5hx <ashishpaudel54@gmail.com>
2024-04-20 20:13:23 +0545

    v0.1

--- src/app/db/page.tsx
+++ src/app/db/page.tsx
@@ -1,20 +1,36 @@
-import React from 'react';
-import Head from 'next/head';
+// import React from 'react';
+// import Head from 'next/head';
 
-const DB: React.FC = () => {
-    return (
-        <div>
-            <Head>
-                <title>Database page</title>
-            </Head>
-            <div>
-                <h1>Database Connection Page</h1>
-                <p>Connecting To databases............................</p>
-                <p></p>
-                <p>Feel free to contact if you encounter any <b>ERROR</b></p> 
-            </div>
-        </div>
-    );
-};
+// const DB: React.FC = () => {
+//     return (
+//         <div>
+//             <Head>
+//                 <title>Database page</title>
+//             </Head>
+//             <div>
+//                 <h1>Database Connection Page</h1>
+//                 <p>Connecting To databases............................</p>
+//                 <p></p>
+//                 <p>Feel free to contact if you encounter any <b>ERROR</b></p> 
+//             </div>
+//         </div>
+//     );
+// };
 
-export default DB;
+// export default DB;
+export default function DB({
+    params,
+    }: {
+        params: {
+            slug: string[];
+        };
+    }) {
+        if(params.slug?.length === 2) {
+            return <h1>DB for feature {params.slug[0]} and concept {params.slug[1]} loading</h1>;
+        }
+        else if(params.slug?.length === 1) {
+            return <h1>docs for feature {params.slug[0]} Loading</h1>;
+        } 
+        return <h1>Docs Home</h1>;
+    }
+    

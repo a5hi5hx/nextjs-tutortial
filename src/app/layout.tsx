6b866711c07163a8f29317e81e4a13f10f01415c
a5hi5hx <ashishpaudel54@gmail.com>
2024-04-21 20:52:14 +0545

    projects page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
                 <ul className="links">
                     <li><a href="profile">Home</a></li>
                     <li><a href="about">About</a></li>
-                    <li><a href="products">Products</a></li>
+                    <li><a href="projects">Projects</a></li>
                     <li><a href="blog">Blog</a></li>
                     <li><a href="contact">Contact</a></li>
                 </ul>
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,20 +1,84 @@
 import React from 'react';
 import Head from 'next/head';
+import styles from './MyProjectsPage.module.css'; // Importing the CSS module for styling
 
-const Projects: React.FC = () => {
+// Sample project data
+const projects = [
+    {
+        id: 1,
+        title: 'Project 1',
+        image: 'https://picsum.photos/200?random=1',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus eget est arcu.',
+        technologies: ['React', 'Next.js', 'CSS'],
+        githubUrl: 'https://github.com/username/project1'
+    },
+    {
+        id: 2,
+        title: 'Project 2',
+        image: 'https://picsum.photos/200?random=2',
+        description: 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.',
+        technologies: ['JavaScript', 'HTML', 'CSS'],
+        githubUrl: 'https://github.com/username/project2'
+    },
+    {
+        id: 3,
+        title: 'Project 3',
+        image: 'https://picsum.photos/200?random=3',
+        description: 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.',
+        technologies: ['JavaScript', 'HTML', 'CSS'],
+        githubUrl: 'https://github.com/username/project2'
+    },{
+        id: 4,
+        title: 'Project 4',
+        image: 'https://picsum.photos/200?random=4',
+        description: 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.',
+        technologies: ['JavaScript', 'HTML', 'CSS'],
+        githubUrl: 'https://github.com/username/project2'
+    },{
+        id: 5,
+        title: 'Project 5',
+        image: 'https://picsum.photos/200?random=5',
+        description: 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.',
+        technologies: ['JavaScript', 'HTML', 'CSS'],
+        githubUrl: 'https://github.com/username/project2'
+    },
+    {
+        id: 6,
+        title: 'Project 6',
+        image: 'https://picsum.photos/200?random=6',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus eget est arcu.',
+        technologies: ['React', 'Next.js', 'CSS'],
+        githubUrl: 'https://github.com/username/project1'
+    },
+    // Add more projects as needed
+];
+
+const MyProjectsPage: React.FC = () => {
     return (
-        <div>
+        <div className={styles.container}>
             <Head>
-                <title>Database page</title>
+                <title>My Projects</title>
             </Head>
-            <div>
-                <h1>DProjects Page</h1>
-                <p>Loading Projects............................</p>
-                <p></p>
-                <p>Feel free to contact if you encounter any <b>ERROR</b></p> 
+            <h1 className={styles.heading}>My Projects</h1>
+            <div className={styles.projectsContainer}>
+                {projects.map(project => (
+                    <div key={project.id} className={styles.projectBox}>
+                        <img src={project.image} alt={project.title} className={styles.projectImage} />
+                        <div className={styles.projectContent}>
+                            <div className={styles.projectTechnologies}>
+                                {project.technologies.map(tech => (
+                                    <span key={tech} className={styles.technology}>{tech}</span>
+                                ))}
+                            </div>
+                            <h2 className={styles.projectTitle}>{project.title}</h2>
+                            <p className={styles.projectDescription}>{project.description}</p>
+                            <a href={project.githubUrl} className={styles.githubLink} target="_blank" rel="noopener noreferrer"><b className={styles.link}>Visit Project</b></a>
+                        </div>
+                    </div>
+                ))}
             </div>
         </div>
     );
 };
 
-export default Projects;
+export default MyProjectsPage;
